61a5079a584773304e568bc4eb7b38b687f1ab1e
Romulo Tone <romulo.tone@etraveligroup.com>
2024-06-25 13:18:30 +0200

    displaying data

--- frontend/src/components/candy-list.tsx
+++ frontend/src/components/candy-list.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { Candy } from "../models/candy-model"
 import { getAllCandy } from "../utils/api-utils"
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box, Button } from "@mui/material";
 
 const CandyList = () => {
   const [candyList, setCandyList] = useState<Candy[]>([])
@@ -26,15 +28,70 @@ const CandyList = () => {
     return <div>Error fetching candy</div>
   }
 
+  const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "name", headerName: "Client Name", flex: 1, align: 'center', headerAlign: 'center'
+    },
+    {
+      field: "candy", headerName: "Candy Name", flex: 1, align: 'center', headerAlign: 'center'
+    },
+    {
+      field: "eaten", headerName: "Amount Eaten", width: 150, align: 'center', headerAlign: 'center'
+    },
+    {
+      field: "date", headerName: "Date", width: 150, align: 'center', headerAlign: 'center', valueGetter: (params) => formatDate(params)
+    },
+  ]
+
   return (
-    <div>
-      {candyList.length > 0 &&
-        candyList.map((candy, index) =>
-          <span key={index}>
-            {candy.candy}<br />
-          </span>)
+    <>
+      {candyList && candyList.length > 0 &&
+        <Box
+          sx={{
+            height: 700,
+            width: 900,
+            backgroundColor: "white",
+            borderRadius: 5,
+            '& .MuiDataGrid-root': {
+              overflow: 'auto',
+              borderRadius: 5,
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              position: 'relative',
+            },
+            '& .MuiDataGrid-footerContainer': {
+              position: 'relative'
+            },
+          }}
+        >
+          <DataGrid
+            rows={candyList.map((row, index) => ({ id: index, ...row, date: new Date(row.date) }))}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 10 },
+              },
+            }}
+            pageSizeOptions={[10, 50, 100]}
+            sx={{
+              borderRadius: '5px',
+            }}
+          />
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
+            <Button variant="contained" onClick={() => console.log('user')}>
+              Aggregate by User
+            </Button>
+          </Box>
+        </Box>
       }
-    </div>
+    </>
   )
 }
 
